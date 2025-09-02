9352ebd5fe6b5c188cbcac47b238b9be9d1739a2
KenzyFrhat <kenzyfrhat723@gmail.com>
2025-09-03 02:17:48 +0300

    Delete function

--- src/DataStructureDecodeCpp/LinkedListImplementation.cpp
+++ src/DataStructureDecodeCpp/LinkedListImplementation.cpp
@@ -40,6 +40,7 @@ class LinkedList {
 public:
 	LinkedListNode* head;
 	LinkedListNode* tail;
+	int length;
 	LinkedListIterator begin() {
 		LinkedListIterator itr(this->head);
 		return itr;
@@ -98,6 +99,90 @@ public:
 
 	}
 
+	void InsertBefore(int newData, int DataAfter) {
+		LinkedListNode* newNode = new LinkedListNode(newData);
+		LinkedListNode* nodeAfter = NULL; 
+		LinkedListNode* nodeBefore = NULL;
+
+		// validation
+		// list is empty -> return  null 
+		if (this->head == NULL)  return ;  //list is empty
+		
+		if (this->head->data == DataAfter) {
+			newNode->next = this->head; // insert as head node
+			this->head = newNode;
+			return; 
+		}
+        
+		// search for existing of the nodeAfter
+		for (LinkedListIterator itr = this->begin(); itr.currentNode->next != NULL; itr.next()) {
+			if (itr.currentNode->next->data == DataAfter) {
+				nodeBefore = itr.currentNode;
+				nodeAfter = itr.currentNode->next;
+			}
+		}
+
+		if (nodeAfter == NULL)  return; //node not found 
+
+		newNode->next = nodeAfter; // insert in the list middle
+		nodeBefore->next = newNode;
+
+	}
+
+	LinkedListNode* FindParent(LinkedListNode* node) {
+		for (LinkedListIterator itr = this->begin(); itr.currentNode->next != NULL; itr.next()) {
+			if (itr.currentNode->next == node) {
+				return itr.currentNode ;
+			}
+		}
+	}
+
+	void DeleteNode(LinkedListNode* node) {
+		if (node == NULL)  return;
+
+		// if length = 1 , node is the head and tail , delete head and tail value
+		if (this->head == this->tail) {
+			this->head = NULL;
+			this->tail = NULL;
+		}
+
+		// node is head node
+		else if (this->head == node) {
+			this->head = node->next;
+		}
+
+		else {
+			//search for parent
+			LinkedListNode* parentNode = FindParent(node);
+			parentNode->next = node->next;
+			// update tail if needed
+			if (this->tail == node) {
+				this->tail = parentNode;
+			}
+		}
+
+		length--;
+
+	}
+
+	void Delete(int data) {
+		//get node 
+		LinkedListNode* node = FindNodeWithData(data);
+		if (node == NULL) return;
+		DeleteNode(node);
+
+	}
+
+	void DeleteHead() {
+		if (this->head == NULL)
+			return;
+		DeleteNode(this->head);
+	}
+
+	void DeleteTail() {
+		if (this->tail == NULL) return;
+		DeleteNode(this->tail);
+	}
 
 };
 
@@ -106,8 +191,22 @@ public:
 int main() {
 
 	LinkedList* list = new LinkedList();
-	list->InsertLast(70);
-	LinkedListNode* node  = list->FindNodeWithData(70);
-	cout << "node data : " << node->data << endl;
-	cout << "node next : " << node->next;
+	list->InsertLast(77);
+	list->PrintList(); cout << endl;
+	list->InsertBefore(22, 77); // 22-> 77
+	list->PrintList(); cout << endl;
+	list->InsertBefore(55, 77); // 22-> 55-> 77
+	list->PrintList(); cout << endl;
+
+	list->InsertBefore(88, 90); // no changing
+	list->PrintList(); cout << " no changing " << endl;
+
+
+	LinkedList* emptyList = new LinkedList();
+	cout << "The head of list2 : " << emptyList->head;
+	emptyList->InsertBefore(99, 100); //no change=ing
+	cout << "The head of list2 after tring adding before : " << emptyList->head;
+
+
+
 }
